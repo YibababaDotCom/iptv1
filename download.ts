af96efe90c0a88cdc570d2470e5748520ebdf442
YibababaDotCom <marriottbelldesk@gmail.com>
2025-06-09 18:26:42 -0400

    Update download.ts

--- download.ts
+++ download.ts
@@ -1,6 +1,6 @@
 async function grabAllChannels () {
     console.log("==> downloading all sources...")
-    const sourceUrl = "https://iptv-org.github.io/iptv/index.m3u";
+    const sourceUrl = "https://raw.githubusercontent.com/Josh9456/IPTV-m3u/refs/heads/main/all_channels/channels.m3u8";
     let reponse = await fetch(sourceUrl);
     let body = await reponse.text();
     let _ = await Deno.writeTextFile("all.m3u", body)
